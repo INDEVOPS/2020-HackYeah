cf0b5204612ee062257bf9478ce314c5a102c780
Lukas Kirylak <lukasz.kirylak@indevops.com>
2020-04-04 11:10:46 +0000

    Disable registration and password recovery refs #44386

--- administration/routes/web.php
+++ administration/routes/web.php
@@ -17,6 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes([
+    'register' => false,
+    'reset' => false,
+]);
 
 Route::get('/home', 'HomeController@index')->name('home');
