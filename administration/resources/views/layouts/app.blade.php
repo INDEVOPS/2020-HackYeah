f24955ecb005cfc5613bc99df7790b8a8b2f3a02
Lukas Kirylak <lukasz.kirylak@indevops.com>
2020-04-04 15:11:55 +0000

    Update navigation bar refs #44389

--- administration/resources/views/layouts/app.blade.php
+++ administration/resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/admin') }}">
                     {{ config('app.name', 'Laravel') }}
@@ -33,7 +33,8 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <a class="nav-link" href="{{ action('QuestionController@index') }}">Questions</a>
+                        <a class="nav-link" href="{{ action('ExportController@index') }}">Export</a>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
